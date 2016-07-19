faca22f8c12043b27ab526a6170b803336319f33
JasonFF <925784658@qq.com>
2016-07-19 20:12:51 +0800

    修改跨域

--- api/routes/index.js
+++ api/routes/index.js
@@ -8,12 +8,13 @@ var newblog = require('./newBlog.js');
 
 module.exports = function(app) {
   app.all('*', function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-      res.header("X-Powered-By",' 3.2.1')
-      res.header("Content-Type", "application/json;charset=utf-8");
-      next();
+    res.set({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+      "Access-Control-Allow-Methods": "PUT,POST,GET,DELETE,OPTIONS",
+      "Content-Type": "application/json;charset=utf-8"
+    })
+    next();
   });
   app.use('/signup', signup);
   app.use('/login', login);
