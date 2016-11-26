35e798daaa21d734bec09a66c81aea2764656924
JasonFF <925784658@qq.com>
2016-11-26 15:17:46 +0800

    完善了一些bug

--- app/components/BlogList/BlogList.jsx
+++ app/components/BlogList/BlogList.jsx
@@ -2,11 +2,31 @@ import React,{Component} from 'react';
 import {Link} from 'react-router';
 import {removeTags} from 'widgets';
 import {connect} from 'react-redux';
+import {action} from 'actions';
 
 const style = require('./BlogList.less');
 
-@connect(state=>({BlogList:state.truck.BlogList}),{})
+@connect(state=>({BlogList:state.truck.BlogList,Blogs:state.truck.Blogs,Notebooks:state.truck.Notebooks}),{action})
 export default class BlogList extends Component {
+    componentWillMount() {
+        const hasId = !!Object.keys(this.props.params).length;
+        if (hasId&&this.props.Notebooks) {
+            this.props.action({
+                moduleName: 'BlogList',
+                goods: {
+                    items: this.props.Notebooks.items[this.props.params.id].data
+                }
+            })
+        }
+        if (!hasId&&this.props.Blogs) {
+            this.props.action({
+                moduleName: 'BlogList',
+                goods: {
+                    items: this.props.Blogs.items
+                }
+            })
+        }
+    }
     render() {
         const {items=[]} = this.props.BlogList||{};
         return <div className={style.blogList}>
@@ -24,7 +44,7 @@ class BlogBox extends Component {
             <h2><Link to={`/blog/${data.id}`}><i className="icon">&#xe66a;</i>{data.title}</Link></h2>
             <div className={style.info}>
                 <li><i className="icon">&#xe625;</i>{data.notebook}</li>
-                <li><i className="icon">&#xe63e;</i>{data.notebook}</li>
+                <li><i className="icon">&#xe63e;</i>{data.time}</li>
             </div>
             <div className={style.digest}>
                 {removeTags(data.html)}
--- app/containers/Hello/Hello.jsx
+++ app/containers/Hello/Hello.jsx
@@ -6,32 +6,12 @@ import {Navbar,BlogList} from 'components';
 
 const style = require('./Hello.less');
 
-@connect(state=>({Blogs: state.truck.Blogs,Notebooks:state.truck.Notebooks}),{action})
 export default class Home extends Component {
-    componentWillMount() {
-        const hasId = !!Object.keys(this.props.params).length;
-        if (hasId&&this.props.Notebooks) {
-            this.props.action({
-                moduleName: 'BlogList',
-                goods: {
-                    items: this.props.Notebooks.items[this.props.params.id].data
-                }
-            })
-        }
-        if (!hasId&&this.props.Blogs) {
-            this.props.action({
-                moduleName: 'BlogList',
-                goods: {
-                    items: this.props.Blogs.items
-                }
-            })
-        }
-    }
     render() {
         return <div className={style.mainContainer}>
             <Navbar type="notebook"></Navbar>
             <div style={{height:'70px'}}></div>
-            <BlogList></BlogList>
+            <BlogList key={JSON.stringify(this.props.params)} params={this.props.params}></BlogList>
         </div>
     }
 }
