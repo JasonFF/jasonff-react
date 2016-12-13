6946bca9272c0b58c81c6bc78209f35d96072f21
JasonFF <925784658@qq.com>
2016-12-13 20:24:11 +0800

    修改了界面

--- app/containers/Hello/Hello.jsx
+++ app/containers/Hello/Hello.jsx
@@ -10,6 +10,7 @@ export default class Home extends Component {
     render() {
         return <div className={style.mainContainer+' '+style.container}>
             <Navbar type="notebook"></Navbar>
+            <img src="/static/image/jsbg.png" className={style.jsbg} alt=""/>
             <BlogList key={JSON.stringify(this.props.params)} params={this.props.params}></BlogList>
         </div>
     }
